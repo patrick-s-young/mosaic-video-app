3f3ef45198a7438827d9bf28b8be4f94553481ba
Patrick S. Young <patrick@bigeyeball.co>
2020-11-28 13:02:20 -0800

    add mosaic render service

--- ffmpeg-api/lib/routes/ffmpeg_routes.ts
+++ ffmpeg-api/lib/routes/ffmpeg_routes.ts
@@ -7,15 +7,20 @@ export class FFmpegRoutes {
   
   public route(app: Application) {
 
-      app.post('/resize', (req: Request, res: Response) => {
+      app.post('/probe', (req: Request, res: Response) => {
          console.log(`req.body.filename: ${req.body.filename}`);
-         //res.status(200).json({message:"/render/resize: Post request successfull"});
-         this.ffmpeg_controller.resize_video(req, res);
+         this.ffmpeg_controller.probe_video(req, res);
       });
 
       app.post('/frames', (req: Request, res: Response) => {
          //res.status(200).json({message:"/render/frames: Post request successfull"});
          this.ffmpeg_controller.export_frames(req, res);
+      });
+
+      app.post('/resize', (req: Request, res: Response) => {
+         console.log(`req.body.filename: ${req.body.filename}`);
+         //res.status(200).json({message:"/render/resize: Post request successfull"});
+         this.ffmpeg_controller.resize_video(req, res);
       });
 
       app.post('/mosaic', (req: Request, res: Response) => {
