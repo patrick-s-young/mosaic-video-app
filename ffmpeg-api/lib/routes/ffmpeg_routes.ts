459b8635e814ad6ce464e6d3d012edc8ad37e388
Patrick S. Young <patrick@bigeyeball.co>
2020-12-05 13:41:53 -0800

    add render and frames export service

--- ffmpeg-api/lib/routes/ffmpeg_routes.ts
+++ ffmpeg-api/lib/routes/ffmpeg_routes.ts
@@ -11,21 +11,27 @@ export class FFmpegRoutes {
          console.log(`req.body.filename: ${req.body.filename}`);
          this.ffmpeg_controller.probe_video(req, res);
       });
-
-      app.post('/frames', (req: Request, res: Response) => {
-         //res.status(200).json({message:"/render/frames: Post request successfull"});
-         this.ffmpeg_controller.export_frames(req, res);
+/*
+      app.post('/frames',
+         (req, res, next) => this.ffmpeg_controller.export_frames(req, res, next),
+         (req, res, next) => {
+            res.status(200).json({ STATUS: res.locals.status });
+         }
       });
+*/
+      app.post('/resize', 
+         (req, res, next) => this.ffmpeg_controller.resize_video(req, res, next),
+         (req, res, next) => this.ffmpeg_controller.export_frames(req, res, next),
+         (req, res, next) => {
+            res.status(200).json({ STATUS: res.locals.status });
+         }
+      );
 
-      app.post('/resize', (req: Request, res: Response) => {
-         console.log(`req.body.filename: ${req.body.filename}`);
-         //res.status(200).json({message:"/render/resize: Post request successfull"});
-         this.ffmpeg_controller.resize_video(req, res);
-      });
-
-      app.post('/mosaic', (req: Request, res: Response) => {
-         //res.status(200).json({message:"/render/mosaic: Post request successfull"});
-         this.ffmpeg_controller.render_mosaic(req, res);
-      });
+      app.post('/mosaic', 
+         (req, res, next) => this.ffmpeg_controller.render_mosaic(req, res, next),
+         (req, res, next) => {
+            res.status(200).json({ STATUS: res.locals.status });
+         }
+      );
    }
 }
