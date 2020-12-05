5ed09e4572ecf9d0bcdda46309b0a77935a845c9
Patrick S. Young <patrick@bigeyeball.co>
2020-12-05 13:38:52 -0800

    add feature redux slices to root reducer

--- client/src/app/rootReducer.ts
+++ client/src/app/rootReducer.ts
@@ -1,11 +1,16 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import  navSlice  from 'features/navigation/navSlice';
-
+import navSlice  from 'features/navigation/navSlice';
+import uploadSlice from 'features/uploadVideo/uploadSlice';
+import scrubberSlice from 'features/mosaicImage/scrubberSlice';
+import mosaicSlice from 'features/mosaicVideo/mosaicSlice';
 import appSlice from 'app/appSlice';
 
 const rootReducer = combineReducers({
   app: appSlice,
-  nav: navSlice
+  nav: navSlice,
+  upload: uploadSlice,
+  scrubber: scrubberSlice,
+  mosaic: mosaicSlice
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
