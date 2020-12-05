07488e162e17d38341a9f421b18eee1057fb0544
Patrick S. Young <patrick@bigeyeball.co>
2020-12-05 13:43:29 -0800

    add render and export frames routing

--- express-server/lib/modules/file/service.ts
+++ express-server/lib/modules/file/service.ts
@@ -1,19 +1,18 @@
 import env from '../../environment';
 import { Request, Response } from 'express';
 const axios = require('axios');
-const { fstat, rename } = require('fs');
+
 
 export default class FileService {
 
     public uploadVideo (req: Request, res: Response, next: any) {
         const uploadVideoDirectory = `${env.getVolumnPath()}/uploads`;
         const myFile = req['files'].myFile;
-        res.locals.filename  = `upload_${Date.now()}.mov`;
+        res.locals.assetID = Date.now();
+        res.locals.filename  = `${res.locals.assetID}.mov`;
         const path = `${uploadVideoDirectory}/${res.locals.filename}`;
         myFile.mv(path, (err) => {
             next();
         });
     }
-
-
 }
--- express-server/lib/routes/render_routes.ts
+++ express-server/lib/routes/render_routes.ts
@@ -1,6 +1,6 @@
 import { Application, Request, Response } from 'express';
 import { RenderController } from '../controllers/renderController';
-
+import env from '../environment';
 
 export class RenderRoutes {
 
@@ -8,10 +8,14 @@ export class RenderRoutes {
   
   public route(app: Application) {
 
-      app.post('/render/mosaic',  
+      app.get('/render/mosaic',  
          (req, res, next) => this.render_controller.render_mosaic(req, res, next), 
          (req, res, next) => {
-         res.status(200).json({message:`/render/mosaic: POST request successfull. Rendered mosaic with movie: ${req.body.filename}`});
+            console.log(`express-server : render_routes : res.locals.response: ${res.locals.response}`)
+            const downloadRenderDirectory = `${env.getVolumnPath()}/public/${req.query.assetID}/mosaic.mov`;
+            res.download(downloadRenderDirectory);
+            
+         //res.status(200).json({message:`/render/mosaic: POST request successfull. Rendered mosaic with movie: ${req.body.filename}`});
       });
    }
 }
