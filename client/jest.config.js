2227b7ef3b9c9f697a43416d138f204a3fbed1d2
Patrick S. Young <patrick@bigeyeball.co>
2020-12-11 09:24:55 -0800

    rename client directory to web-app

--- client/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  moduleNameMapper: {
-    "\\.css$": "<rootDir>/__mocks__/styleMock.js"
-  },
-  resolver: "jest-webpack-resolver",
-  setupFilesAfterEnv: ['<rootDir>/src/setUpTests.js']
-}
--- client/src/features/mosaicVideo/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-export { getCopyVideoFromArea } from 'features/mosaicVideo/helpers/getCopyVideoFromArea';
-export { getInPoints } from 'features/mosaicVideo/helpers/getInPoints';
-export { getDrawToCanvasArea } from 'features/mosaicVideo/helpers/getDrawToCanvasArea';
-export { getTileAnimEvents } from 'features/mosaicVideo/helpers/getTileAnimEvents';
-export { getMosaicSelectorConfig } from 'features/mosaicVideo/helpers/getMosaicSelectorConfig';
